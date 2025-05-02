a3c3ee743404f80272637e88880fdfbc71b2a62b
flerbde <flerbde@gmail.com>
2025-05-02 16:53:22 +0200

    Delete public directory

--- public/firebase-firestore.js
+++
@@ -1,2 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, doc, getDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"; 
